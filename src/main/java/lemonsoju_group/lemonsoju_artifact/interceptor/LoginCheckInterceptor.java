15f861674eab513617a74be50d24802b528e9cf8
Lemon Soju <yckk980320@naver.com>
2022-03-06 13:24:30 +0900

    Add Basket Delete Function 02

--- src/main/java/lemonsoju_group/lemonsoju_artifact/interceptor/LoginCheckInterceptor.java
+++ src/main/java/lemonsoju_group/lemonsoju_artifact/interceptor/LoginCheckInterceptor.java
@@ -1,2 +1,29 @@
-package lemonsoju_group.lemonsoju_artifact.interceptor;public class LoginCheckInterceptor {
+package lemonsoju_group.lemonsoju_artifact.interceptor;
+
+import lemonsoju_group.lemonsoju_artifact.SessionConst;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Slf4j
+public class LoginCheckInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        String requestURI = request.getRequestURI();
+        log.info("authorization check intercept start {}", requestURI);
+
+        HttpSession session = request.getSession();
+        if (session == null || session.getAttribute(SessionConst.LOGIN_USER) == null){
+            log.info("unauthorized user request");
+            //로그인으로 redirect
+            response.sendRedirect("/login?redirectURL=" + requestURI);
+            return false;
+        }
+        return true;
+    }
 }
